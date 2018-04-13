3aeeb92ec6fa0199c51afbbd75d55e7f9a70a363
Christopher Dearie <christopher.j.dearie@nordstrom.com>
2018-04-13 15:23:23 -0700

    update dev branch

--- resourceStreamer/run.py
+++ resourceStreamer/run.py
@@ -1,6 +1,9 @@
 import os
 import logging
+import sys
 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../venv/Lib/site-packages')))
+import requests
 from azure.mgmt.resource import ResourceManagementClient
 from azure.common.credentials import ServicePrincipalCredentials
 
@@ -12,10 +15,10 @@ SUBSCRIPTIONS = os.getenv("subscription_list", ["54dd3907-ccd0-4338-963e-6a58a10
 SECRET = os.getenv("AZURE_SECRET")
 AZURE_CLIENT_ID = os.getenv("AZURE_CLIENT_ID")
 AZURE_TENANT_ID = os.getenv("AZURE_TENANT_ID")
+SP_SECRET_URI = os.getenv("SP_SECRET_URI")
 
 
 def list_resource_groups(subscription_id):
-    
     creds = get_service_principal_cred()
     client = ResourceManagementClient(credentials=creds, subscription_id=subscription_id)
     res = client.resource_groups.list()
@@ -27,16 +30,6 @@ def list_resource_groups(subscription_id):
 
     return resource_group_list
 
-def get_service_principal_cred():
-    logger.debug("Retrieving a ServicePrincipal credential")
-
-    creds = ServicePrincipalCredentials(
-        client_id = os.getenv('AZURE_CLIENT_ID'),
-        secret=SECRET,
-        tenant=os.getenv('AZURE_TENANT_ID')
-    )
-
-    return creds
 
 def main():
     print("starting...")
@@ -47,8 +40,39 @@ def main():
         for rg in res:
             print(rg)
 
-main()
+
+def get_kv_api_token():
+    MSI_ENDPOINT = os.getenv("MSI_ENDPOINT")
+    MSI_SECRET = os.getenv("MSI_SECRET")
+
+    RESOURCE_URI = "https://vault.azure.net"
+    TOKEN_AUTH_URI = "{0}?resource={1}&api-version=2017-09-01".format(MSI_ENDPOINT, RESOURCE_URI)
+
+    headers = {"Secret": MSI_SECRET}
+
+    res = requests.get(TOKEN_AUTH_URI, headers=headers)
+    jres = res.json()
+    return jres["access_token"]
 
 
+def get_sp_secret():
+    token = get_kv_api_token()
+    SECRET_URI = "{0}?api-version=2016-10-01".format(SP_SECRET_URI)
 
+    headers = {"Authorization": "Bearer {0}".format(token)}
+    res = requests.get(SECRET_URI, headers=headers)
+    jres = res.json()
+    return jres["value"]
+
+
+def get_service_principal_cred():
+    logger.debug("Retrieving a ServicePrincipal credential")
+
+    creds = ServicePrincipalCredentials(
+        client_id=os.getenv('AZURE_CLIENT_ID'),
+        secret=get_sp_secret(),
+        tenant=os.getenv('AZURE_TENANT_ID')
+    )
+
+    return creds
 
